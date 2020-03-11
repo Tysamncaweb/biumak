fdd0d4229cc41663bdf831318978c1c0432dbe23
Alejandro Sanchez <asanchez@tysamnca.com>
2020-03-11 13:53:53 -0400

    Se elimina validación de números únicos para facturas

--- localizacion/l10n_ve_fiscal_requirements/model/invoice.py
+++ localizacion/l10n_ve_fiscal_requirements/model/invoice.py
@@ -79,24 +79,23 @@ class AccountInvoice(models.Model):
         return res and res[0] or False
 
 
-    def _unique_invoice_per_partner(self, field, value):
-        """ Return false when it is found
-        that the bill is not out_invoice or out_refund,
-        and it is not unique to the partner.
-        """
+    #def _unique_invoice_per_partner(self, field, value):
+    #    """ Return false when it is found
+    #    that the bill is not out_invoice or out_refund,
+    #    and it is not unique to the partner.
+    #    """
+    #    ids_ivo = []
+    #    inv_ids = []
+    #    for inv in self:
 
-        ids_ivo = []
-        inv_ids = []
-        for inv in self:
+    #        ids_ivo.append(inv.id)
+    #        if inv.type in ('out_invoice', 'out_refund'):
+    #            return True
+    #        inv_ids = (self.search([(field, '=',value), ('type', '=', inv.type), ('partner_id', '=', inv.partner_id.id)]))
 
-            ids_ivo.append(inv.id)
-            if inv.type in ('out_invoice', 'out_refund'):
-                return True
-            inv_ids = (self.search([(field, '=',value), ('type', '=', inv.type), ('partner_id', '=', inv.partner_id.id)]))
-
-            if [True for i in inv_ids if i not in ids_ivo] and inv_ids:
-                return False
-        return True
+    #        if [True for i in inv_ids if i not in ids_ivo] and inv_ids:
+    #            return False
+    #    return True
 
     def _get_loc_req(self):
         """Get if a field is required or not by a Localization
@@ -169,40 +168,40 @@ class AccountInvoice(models.Model):
         if vals.get('type') in ('out_invoice', 'out_refund') and \
                 vals.get('date_invoice') and not vals.get('date_document'):
             vals['date_document'] = vals['date_invoice']
-        if vals.get('supplier_invoice_number', False):
-            supplier_invoice_number_id = self._unique_invoice_per_partner('supplier_invoice_number',
-                                                                          vals.get('supplier_invoice_number', False))
-            if not supplier_invoice_number_id:
-                self.supplier_invoice_number = False
-                return {'warning': {'title': "Advertencia!",
-                                    'message': "  El Numero de la Factura del Proveedor ya Existe  "}}
-        if vals.get('nro_ctrl', False):
-            nro_ctrl_id = self._unique_invoice_per_partner('nro_ctrl', vals.get('nro_ctrl', False))
-            if not nro_ctrl_id:
-                self.nro_ctrl = False
-                return {'warning': {'title': "Advertencia!",
-                                    'message': "  El Numero de control de la Factura del Proveedor ya Existe  "}}
+        #if vals.get('supplier_invoice_number', False):
+        #    supplier_invoice_number_id = self._unique_invoice_per_partner('supplier_invoice_number',
+        #                                                                  vals.get('supplier_invoice_number', False))
+        #    if not supplier_invoice_number_id:
+        #        self.supplier_invoice_number = False
+        #        return {'warning': {'title': "Advertencia!",
+        #                            'message': "  El Numero de la Factura del Proveedor ya Existe  "}}
+        #if vals.get('nro_ctrl', False):
+        #    nro_ctrl_id = self._unique_invoice_per_partner('nro_ctrl', vals.get('nro_ctrl', False))
+        #    if not nro_ctrl_id:
+        #        self.nro_ctrl = False
+        #        return {'warning': {'title': "Advertencia!",
+        #                            'message': "  El Numero de control de la Factura del Proveedor ya Existe  "}}
 
 
         return super(AccountInvoice, self).write(vals)
 
-    @api.onchange('supplier_invoice_number')
-    def onchange_supplier_invoice_number(self):
-        if self.supplier_invoice_number:
-            supplier_invoice_number_id = self._unique_invoice_per_partner('supplier_invoice_number', self.supplier_invoice_number)
-            if not supplier_invoice_number_id:
-                self.supplier_invoice_number = False
-                return {'warning': {'title': "Advertencia!",
-                                    'message': "  El Numero de la Factura del Proveedor ya Existe  "}}
+    #@api.onchange('supplier_invoice_number')
+    #def onchange_supplier_invoice_number(self):
+    #    if self.supplier_invoice_number:
+    #        supplier_invoice_number_id = self._unique_invoice_per_partner('supplier_invoice_number', self.supplier_invoice_number)
+    #        if not supplier_invoice_number_id:
+    #            self.supplier_invoice_number = False
+    #            return {'warning': {'title': "Advertencia!",
+    #                                'message': "  El Numero de la Factura del Proveedor ya Existe  "}}
 
-    @api.onchange('nro_ctrl')
-    def onchange_nro_ctrl(self):
-        if self.nro_ctrl:
-            nro_ctrl_id = self._unique_invoice_per_partner('nro_ctrl',self.nro_ctrl)
-            if not nro_ctrl_id:
-                self.nro_ctrl = False
-                return {'warning': {'title': "Advertencia!",
-                                    'message': "  El Numero de control de la Factura del Proveedor ya Existe  "}}
+    #@api.onchange('nro_ctrl')
+    #def onchange_nro_ctrl(self):
+    #    if self.nro_ctrl:
+    #        nro_ctrl_id = self._unique_invoice_per_partner('nro_ctrl',self.nro_ctrl)
+    #        if not nro_ctrl_id:
+    #            self.nro_ctrl = False
+    #            return {'warning': {'title': "Advertencia!",
+    #                                'message': "  El Numero de control de la Factura del Proveedor ya Existe  "}}
 
 class AccountInvoiceTax(models.Model):
     _inherit = 'account.invoice.tax'
